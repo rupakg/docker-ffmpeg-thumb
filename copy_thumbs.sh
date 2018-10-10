f04f1de58323c06b03678472544bb5ad89088e25
Rupak Ganguly <rupakg@gmail.com>
2018-10-10 15:33:34 -0400

    Revert "Update copy command to make the file read-only for all"
    This reverts commit d1ce76d971c8c6e589c8ebef4320a2c66fb0d3ab.

--- copy_thumbs.sh
+++ copy_thumbs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Copying ${OUTPUT_THUMBS_FILE_NAME} to S3 at ${OUTPUT_S3_PATH}/${OUTPUT_THUMBS_FILE_NAME} ..."
-aws s3 cp ./${OUTPUT_THUMBS_FILE_NAME} s3://${OUTPUT_S3_PATH}/${OUTPUT_THUMBS_FILE_NAME} --region ${AWS_REGION} --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 cp ./${OUTPUT_THUMBS_FILE_NAME} s3://${OUTPUT_S3_PATH}/${OUTPUT_THUMBS_FILE_NAME} --region ${AWS_REGION}
